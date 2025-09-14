4b75e1480debb20d326ea3b265c25b8dff45e5d6
sijunkim <papaya9349@naver.com>
2025-09-15 01:14:19 +0900

    refactor: Delete dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-
 import { Server } from "@modelcontextprotocol/sdk/server/index.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import {
